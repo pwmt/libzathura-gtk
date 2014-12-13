b8c8f3c25c895404ca0ff6e84b790974bbfcf578
Moritz Lipp <mlq@pwmt.org>
2014-12-13 03:15:08 +0100

    Initialize gtk in test/main

--- tests/tests.c
+++ tests/tests.c
@@ -1,16 +1,21 @@
 /* See LICENSE file for license and copyright information */
 
+#include <gtk/gtk.h>
 #include <check.h>
 #include <fiu.h>
 
 Suite* suite_document(void);
 
-int main(void)
+int
+main(int argc, char* argv[])
 {
   /* initialize libfiu */
 #ifdef FIU_ENABLE
   fiu_init(0);
 #endif
+
+  /* Initialize GTK */
+  gtk_init(&argc, &argv);
 
   /* setup test suite */
   SRunner* suite_runner = srunner_create(NULL);
