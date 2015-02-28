ef708ce9190e4c3723a681faa0779c5858fe3c0d
Moritz Lipp <mlq@pwmt.org>
2015-02-28 14:33:21 +0100

    Respect scroll setting for text fields

--- libzathura-gtk/page/form-fields/form-field-text.c
+++ libzathura-gtk/page/form-fields/form-field-text.c
@@ -9,16 +9,8 @@ static gboolean cb_form_field_text_multiline_focus_out(GtkWidget* widget, GdkEve
 struct _ZathuraFormFieldTextPrivate {
   zathura_form_field_t* form_field;
 
-  union {
-    struct {
-      GtkWidget* text_widget;
-    } single;
-
-    struct {
-      GtkWidget* text_widget;
-      GtkWidget* scrolled_window;
-    } multiline;
-  } gtk;
+  GtkWidget* text_widget;
+  GtkWidget* scrolled_window;
 };
 
 G_DEFINE_TYPE_WITH_PRIVATE(ZathuraFormFieldText, zathura_gtk_form_field_text, GTK_TYPE_BIN)
@@ -38,7 +30,7 @@ zathura_gtk_form_field_text_init(ZathuraFormFieldText* widget)
   ZathuraFormFieldTextPrivate* priv = ZATHURA_FORM_FIELD_TEXT_GET_PRIVATE(widget);
 
   priv->form_field  = NULL;
-  priv->gtk.single.text_widget = NULL;
+  priv->text_widget = NULL;
 
   gtk_widget_add_events(GTK_WIDGET(widget), GDK_BUTTON_PRESS_MASK);
 }
@@ -77,37 +69,47 @@ zathura_gtk_form_field_text_new(zathura_form_field_t* form_field)
           return NULL;
         }
 
-        priv->gtk.single.text_widget = gtk_entry_new();
-        gtk_entry_set_has_frame(GTK_ENTRY(priv->gtk.single.text_widget), FALSE);
-        gtk_entry_set_max_length(GTK_ENTRY(priv->gtk.single.text_widget), max_length);
-        gtk_entry_set_visibility(GTK_ENTRY(priv->gtk.single.text_widget), !is_password);
+        priv->text_widget = gtk_entry_new();
+        gtk_entry_set_has_frame(GTK_ENTRY(priv->text_widget), FALSE);
+        gtk_entry_set_max_length(GTK_ENTRY(priv->text_widget), max_length);
+        gtk_entry_set_visibility(GTK_ENTRY(priv->text_widget), !is_password);
 
-        g_signal_connect(priv->gtk.single.text_widget, "focus-out-event",
+        g_signal_connect(priv->text_widget, "focus-out-event",
             G_CALLBACK(cb_form_field_text_focus_out),
             widget);
 
         if (text != NULL) {
-          gtk_entry_set_text(GTK_ENTRY(priv->gtk.single.text_widget), text);
+          gtk_entry_set_text(GTK_ENTRY(priv->text_widget), text);
         }
-        gtk_container_add(GTK_CONTAINER(widget), priv->gtk.single.text_widget);
+
+        gtk_container_add(GTK_CONTAINER(widget), priv->text_widget);
       }
       break;
     case ZATHURA_FORM_FIELD_TEXT_TYPE_MULTILINE:
       {
-        priv->gtk.multiline.text_widget = gtk_text_view_new();
-        GtkTextBuffer* buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(priv->gtk.multiline.text_widget));
+        priv->text_widget = gtk_text_view_new();
+        GtkTextBuffer* buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(priv->text_widget));
 
         if (text != NULL) {
           gtk_text_buffer_set_text(buffer, text, -1);
         }
 
-        g_signal_connect(priv->gtk.multiline.text_widget, "focus-out-event",
+        g_signal_connect(priv->text_widget, "focus-out-event",
             G_CALLBACK(cb_form_field_text_multiline_focus_out),
             widget);
 
-        priv->gtk.multiline.scrolled_window = gtk_scrolled_window_new(NULL, NULL);
-        gtk_container_add(GTK_CONTAINER(priv->gtk.multiline.scrolled_window), priv->gtk.multiline.text_widget);
-        gtk_container_add(GTK_CONTAINER(widget), priv->gtk.multiline.scrolled_window);
+        bool do_scroll;
+        if (zathura_form_field_text_do_scroll(priv->form_field, &do_scroll) != ZATHURA_ERROR_OK) {
+          return NULL;
+        }
+
+        if (do_scroll == true) {
+          priv->scrolled_window = gtk_scrolled_window_new(NULL, NULL);
+          gtk_container_add(GTK_CONTAINER(priv->scrolled_window), priv->text_widget);
+          gtk_container_add(GTK_CONTAINER(widget), priv->scrolled_window);
+        } else {
+          gtk_container_add(GTK_CONTAINER(widget), priv->text_widget);
+        }
       }
       break;
     case ZATHURA_FORM_FIELD_TEXT_TYPE_FILE_SELECT:
@@ -128,7 +130,7 @@ cb_form_field_text_focus_out(GtkWidget* widget, GdkEventFocus* UNUSED(event),
 
   ZathuraFormFieldTextPrivate* priv = ZATHURA_FORM_FIELD_TEXT_GET_PRIVATE(form_field_widget);
 
-  if (zathura_form_field_text_set_text(priv->form_field, gtk_entry_get_text(GTK_ENTRY(priv->gtk.single.text_widget))) != ZATHURA_ERROR_OK) {
+  if (zathura_form_field_text_set_text(priv->form_field, gtk_entry_get_text(GTK_ENTRY(priv->text_widget))) != ZATHURA_ERROR_OK) {
     return FALSE;
   }
 
@@ -144,7 +146,7 @@ cb_form_field_text_multiline_focus_out(GtkWidget* widget, GdkEventFocus* UNUSED(
 
   ZathuraFormFieldTextPrivate* priv = ZATHURA_FORM_FIELD_TEXT_GET_PRIVATE(form_field_widget);
 
-  GtkTextBuffer* buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(priv->gtk.multiline.text_widget));
+  GtkTextBuffer* buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(priv->text_widget));
   GtkTextIter start, end;
   gtk_text_buffer_get_bounds(buffer, &start, &end);
 
